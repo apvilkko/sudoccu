b4f3276d2ab513242b436d3f2c16385076afa5dd
Antti Pekka Vilkko <apvilkko@gmail.com>
2019-08-28 09:30:38 +0300

    svelte version

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { initBoard } from "./engine/logic";
+  import SudokuBoard from "./SudokuBoard.svelte";
+  let board = initBoard();
+
+  const generate = () => {
+    board.randomize();
+    board = board;
+  };
+</script>
+
+<div>
+  <h1>Sudoccu</h1>
+  <button on:click={generate}>Generate</button>
+  <SudokuBoard {board} />
+</div>
---
+++ my-svelte-project/src/SudokuBoard.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let board;
+</script>
+
+<div class="board">
+  {#each Array.from({ length: board.size() }) as a, i}
+    {#each board.row(i) as item}
+      <div
+        class={`cell ${item.y % 3 === 2 ? 'region-y' : ''} ${item.x % 3 === 2 ? 'region-x' : ''}`}>
+        <span class="value">{item.value}</span>
+      </div>
+    {/each}
+  {/each}
+</div>
